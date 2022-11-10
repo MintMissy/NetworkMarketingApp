8c94886f1d89aa4b427f9c9a0187a1f015934693
Mint Missy <dawidkostka480@gmail.com>
2022-11-10 21:31:23 +0100

    Add project structure

---
+++ src/app/core/components/not-found-page/not-found-page.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-not-found-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './not-found-page.component.html',
+  styleUrls: ['./not-found-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NotFoundPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
