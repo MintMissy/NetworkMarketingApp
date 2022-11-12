06c842a204c6d2e2bb3e9f362870d5424bac0a27
Mint Missy <dawidkostka480@gmail.com>
2022-11-12 15:18:25 +0100

    Lazy load components and increase initial budget size

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BusinessOwnersComponent } from './business-owner/feature/business-owners/business-owners.component';
-import { BusinessesComponent } from './business/feature/businesses/businesses.component';
 import { LanguageConfigurationComponent } from './configuration/feature/language-configuration/language-configuration.component';
 import { HomeComponent } from './core/components/home/home.component';
 import { NotFoundPageComponent } from './core/components/not-found-page/not-found-page.component';
@@ -10,11 +9,37 @@ import { ShopsComponent } from './shop/feature/shops/shops.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'businesses', component: BusinessesComponent },
-  { path: 'owners', component: BusinessOwnersComponent },
-  { path: 'shops', component: ShopsComponent },
-  { path: 'credits', component: CreditsComponent },
-  { path: 'configuration/language', component: LanguageConfigurationComponent },
+  {
+    path: 'businesses',
+    loadComponent: () =>
+      import('./business/feature/businesses/businesses.component').then(
+        (c) => c.BusinessesComponent
+      ),
+  },
+  {
+    path: 'owners',
+    loadComponent: () =>
+      import('./business-owner/feature/business-owners/business-owners.component').then(
+        (c) => c.BusinessOwnersComponent
+      ),
+  },
+  {
+    path: 'shops',
+    loadComponent: () =>
+      import('./shop/feature/shops/shops.component').then((c) => c.ShopsComponent),
+  },
+  {
+    path: 'credits',
+    loadComponent: () =>
+      import('./credits/feature/credits/credits.component').then((c) => CreditsComponent),
+  },
+  {
+    path: 'configuration/language',
+    loadComponent: () =>
+      import(
+        './configuration/feature/language-configuration/language-configuration.component'
+      ).then((c) => LanguageConfigurationComponent),
+  },
   { path: '404', component: NotFoundPageComponent },
   { path: '**', redirectTo: '404' },
 ];
