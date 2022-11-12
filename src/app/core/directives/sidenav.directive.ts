8f99d82c6ed48c7ab7d7404c3c1ce6fe1fc6f8f5
Mint Missy <dawidkostka480@gmail.com>
2022-11-12 14:23:28 +0100

    Responsive changes to sidebar

--- src/app/core/directives/sidenav.directive.ts
+++ src/app/core/directives/sidenav.directive.ts
@@ -1,25 +1,49 @@
-import { Directive, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
-import { MatDrawer, MatSidenav } from '@angular/material/sidenav';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { MatDrawer, MatDrawerContainer, MatSidenav } from '@angular/material/sidenav';
 
 @Directive({
   selector: '[appSidenav]',
 })
-export class SidenavDirective {
-  @Input() sidenav!: MatDrawer;
-  constructor(private element: ElementRef) {}
+export class SidenavDirective implements OnInit {
+  @Input() drawerContainer!: MatDrawerContainer;
+  @Input() drawer!: MatDrawer;
+  private innerWidth: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.setInnerWidth(window.innerWidth);
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: UIEvent): void {
     // TODO close sidenav after clicking in the button
-    // TODO add backdrop shadow that will close sidenav when innerWidth < 500
-    // TODO move this logic to directive
     // @ts-ignore
-    if (event.target.innerWidth < 500) {
-      this.sidenav.mode = 'over';
-      this.sidenav.close();
+    this.setInnerWidth(event.target.innerWidth);
+  }
+
+  private setInnerWidth(newWidth: number): void {
+    this.innerWidth = newWidth;
+    this.updateElementView();
+  }
+
+  private updateElementView(): void {
+    if (this.innerWidth < 600) {
+      this.drawer.mode = 'over';
+      this.drawer.close();
+      this.drawerContainer.hasBackdrop = true;
     } else {
-      this.sidenav.mode = 'side';
-      this.sidenav.open();
+      this.drawer.mode = 'side';
+      this.drawer.open();
+      this.drawerContainer.hasBackdrop = false;
     }
   }
 }
